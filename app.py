9f248bf66c9aa75ac5c414c5bff231487a57c68e
russell-c <russell.coke12@gmail.com>
2018-11-21 16:36:05 -0500

    getting started

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import render_template, Flask, request, session, url_for, redirect
+import pymysql.cursors
+
+app = Flask(__name__)
+
+conn = pymysql.connect(host='localhost',
+                       port=8889,
+                       user='russell',
+                       password='root',
+                       db='PriCoSha',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor
+                    )
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/loginAuth', methods=['GET', 'POST'])
+def loginAuth():
+    email = request.form['email']
+    password = request.form['password']
+
+    cursor = conn.cursor()
+
+    query = 'SELECT * FROM person WHERE email = %s AND password = %s'
+    cursor.execute(query, (email, password))
+    data = cursor.fetchone()
+
+    cursor.close()
+    error = None
+
+    if(data):
+        session['email'] = email
+        return redirect(url_for('home'))
+    else:
+        error = 'Invalid email address or password'
+        return render_template('login.html', error=error)
+
+@app.route('/home')
+def home():
+    user = session['email']
+    print(user)
+    return render_template('home.html', user=user)
+
+app.secret_key = 'secret :)'
+if __name__ == "__main__":
+    app.run('127.0.0.1', 5000, debug = True)
