ba723985caf8d0718a35228d321bc0ba005f6654
russell-c <russell.coke12@gmail.com>
2018-11-21 16:46:54 -0500

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 conn = pymysql.connect(host='localhost',
                        port=8889,
-                       user='russell',
+                       user='root',
                        password='root',
                        db='PriCoSha',
                        charset='utf8mb4',
